553a101626095fdcab71c49664b4790b69cd9ae8
looitavo <gustavobonfim0703@gmail.com>
2026-08-10 21:53:10 -0300

    Add files via upload

---
+++ Sistema_Academico2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_academico2;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Sistema_Academico2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
